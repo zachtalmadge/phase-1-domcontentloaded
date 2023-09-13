6fa295b37a2803566f2e99ee9b27e74e5b60aaaa
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-13 11:29:06 -0400

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let element = document.querySelector('p');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    element.textContent = "This is really cool!"
+    
+})
